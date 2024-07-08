0030c06faec2f9d637a7a0184611a95f19664c1c
jeffrey-zang <jeffrey.zang.07@gmail.com>
2024-07-08 13:25:59 -0400

    deps: install and config radix-ui

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,5 +1,3 @@
-// import { useState } from 'react'
-
 const Results = () => {
   return <div>Results</div>;
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
+import "./styles.css";
+import { Theme } from '@radix-ui/themes';
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Theme
+    accentColor="blue"
+    grayColor="sand"
+    radius="medium"
+      
+  >
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Theme>,
 );
